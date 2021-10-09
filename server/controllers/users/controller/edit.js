6c8cbc91b6c34a15ddc21b654ca599250e6bffbe
kangseokho <kjssaa3@gmail.com>
2021-10-09 21:12:43 +0900

    feature/usercontroller

--- server/controllers/users/controller/edit.js
+++ server/controllers/users/controller/edit.js
@@ -8,27 +8,31 @@ module.exports = async(req, res) => {
         res.status(403).json({ data: { userInfo : null }, message: "authorization is required" })
     }else{
         //해독하여 데이터베이스확인 후
-        const verify = jwt.verify(token,process.env.ACCESS_SECRET);
-        const userInfo = await user.findOne({
-            where : { id : verify.id}
-        })
-        //유저정보 update로 수정. 이때 req.body.user_nickname,req.body.user_password 변경
-        if(!userInfo){
-            res.status(401).json()
+        const verify = jwt.verify(token,process.env.ACCESS_SECRET); //verify 안됐을시 
+        if(!verify){
+            res.status(401).json({ data: { userInfo : null }, message: "authorization is required" })
         }else{
-            //유저정보 있으면
-            const update = await user.update({
-                user_nickname : req.body.user_nickname,
-                user_password : req.body.user_password
-            },
-                {
-                    where : { id : userInfo.id}
-                } 
-            )
-            delete update.dataValues.user_password
-            res.status(200).json({ data : {userInfo : update}, message : "ok"})
+            const userInfo = await user.findOne({
+                where : { id : verify.id} //
+            })
+            if(!userInfo){
+                res.status(401).json({ data: { userInfo : null }, message: "authorization is required" }) //추가
+            }else{
+                //유저정보 있으면
+                await user.update({
+                    user_nickname : req.body.user_nickname,
+                    user_password : req.body.user_password
+                },
+                    {
+                        where : { id : userInfo.id}
+                    } 
+                )
+                
+                delete userInfo.dataValues.user_password;
+                res.status(200).json({ data : {userInfo : userInfo.dataValues}, message : "ok"})
+            }
+
         }
+        //유저정보 update로 수정. 이때 req.body.user_nickname,req.body.user_password 변경
     }
-   
-   
 }
